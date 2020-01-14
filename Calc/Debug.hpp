9e5e8b7c79a4932225acbf0e8d435c8f56180ed2
wheelercj <chrisw289@gmail.com>
2020-01-13 22:19:25 -0800

    Improve debug logger
    Function logError now returns the error message, allowing it (the
    error message) to be thrown in the same line of code as the
    logError function call.

--- Calc/Debug.hpp
+++ Calc/Debug.hpp
@@ -5,17 +5,18 @@
 #define DEBUG_LEVEL 0
 
 #if DEBUG_LEVEL == 0
-	#define LOG(message); ;
+	#define LOG(message) message
 #elif DEBUG_LEVEL == 1
-	#define LOG(message); logError(__LINE__, __FILENAME__, message);
+	#define LOG(message) logError(__LINE__, __FILENAME__, message)
 	#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
 #endif
 #undef DEBUG_LEVEL
 
-void logError(int line, const char* fileName, const std::string& message)
+std::string logError(int line, const char* fileName, const std::string& message)
 {
 	std::ofstream logFile;
 	logFile.open("debug_log.txt", std::ios::app);
 	logFile << "\n- " << __TIME__ << " \"" << message << "\" from line " << line << " in file " << fileName;
 	logFile.close();
+	return message;
 }
